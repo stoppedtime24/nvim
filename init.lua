4496d66d3e512e9d1e395df73304bd5047a76d40
John Fitzgerald Nocum <johnfitzgerald0310@gmail.com>
2025-03-15 19:19:24 +0000

    Added lualine

--- init.lua
+++ init.lua
@@ -89,6 +89,14 @@ require('lazy').setup({
     end
   },
 
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup {}
+      end,
+  },
+
   -- File explorer
   {
     'nvim-tree/nvim-tree.lua',
