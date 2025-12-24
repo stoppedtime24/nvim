7e71cef385f4adc0ce02aac660c2663ff9c46a50
John Fitzgerald Nocum <johnfitzgerald0310@gmail.com>
2025-12-24 01:18:28 +0000

    Added keymaps to cokeline

--- lua/stoppedtime24/plugins/cokeline.lua
+++ lua/stoppedtime24/plugins/cokeline.lua
@@ -5,5 +5,18 @@ return {
     "nvim-tree/nvim-web-devicons",
     "stevearc/resession.nvim"
   },
-  config = true,
+  config = function()
+    require('cokeline').setup()
+    
+    local map = vim.api.nvim_set_keymap
+    map("n", "<Leader>fpb", "<Plug>(cokeline-focus-prev)", { silent = true })
+    map("n", "<Leader>fnb", "<Plug>(cokeline-focus-next)", { silent = true })
+    map("n", "<Leader>spb", "<Plug>(cokeline-switch-prev)", { silent = true })
+    map("n", "<Leader>snb", "<Plug>(cokeline-switch-next)", { silent = true })
+    map("n", "<Leader>bc", "<Plug>(cokeline-pick-close)", { silent = true })
+    
+    for i = 1, 9 do
+      map("n", ("<Leader>%s"):format(i), ("<Plug>(cokeline-focus-%s)"):format(i), { silent = true })
+    end
+  end,
 }
